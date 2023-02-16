134b47e06af893dd0c5be3659950c88acee5ea06
Gloria Reina <gloria.l.reina11@gmail.com>
2023-02-16 11:06:14 -0500

    Finally! survived taco soup

--- main.js
+++ main.js
@@ -93,11 +93,106 @@ Write a loop that counts by 2's and creates the following output.
 Output
 2 4 6 8 Who do we appreciate?!*/
 
-let numbersArray = [1,2,3,4,5,6,7,8,9]
+/*let numbersArray = [1,2,3,4,5,6,7,8,9]
+
+let evennumberArray= []
+
 for(let i = 0; i < numbersArray.length; i++){
-    if(numbersArray[i]/ 2 === 0){
-        console.log(numbersArray)
-    } else {
-        console.log("This is an odd number")
+    if(numbersArray[i]% 2 === 0){
+        evennumberArray+= numbersArray[i]
     }
 }
+console.log(evennumberArray + " " + "Who do we appreciate")*/
+
+/* alternate attempt
+for(let i = 2; i < 10; i+=2){
+    console.log(i)
+    }
+console.log("Who do we appreciate")*/
+
+/*Interrupting Cow Exercise:
+Write a loop that builds sentenceArray into a real sentence, except every fourth word should be "MOOOOOO". Hint: look up the modulus operator.*/
+
+var sentenceArray = ["the", "cow", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+
+/*for(let word= 0; word < sentenceArray.length; word++){
+    if([word]% 2 === 0){
+        console.log("MOOOOOO")
+    } else if([word]% 4 !== 0){
+        console.log(sentenceArray[word])
+    }
+}*/
+
+//Exercise Beatles:Output the following values
+
+//${name} was in the Beatles from ${year} to ${year}. He was born in ${year}. He contributed heavily to the ${albumName} Album.Please note that these values are not correct. They're just placeholders that you will need to correctly fill out. 
+
+const beatles = {
+    albums: ["Abbey Road", "Sgt Peppers Lonely Heart's Club Band", "Revolver", "Magical Mystery Tour"],
+    history: {
+        formed: 1960,
+        disbanded: 1970
+    },
+    members: [
+        {
+            name: "George Harrison",
+            birth: 1943,
+            death: 2001
+        },
+        {
+            name: "Paul McCartney",
+            birth: 1942,
+            death: null
+        },
+        {
+            name: "John Lennon",
+            birth: 1940,
+            death: 1980
+        },
+        {
+            name: "Ringo Starr",
+            birth: 1940,
+            death: null
+        }
+    ]
+}
+
+let key1= "members"
+let key2= "history"
+let key3= "albums"
+
+// Access band member 1 (aka George) info: need name, birth year, year started and ended band career
+let bandMember1= beatles[key1][0]
+let bandMember1Name= bandMember1.name
+let bandMember1Birth= bandMember1.birth
+let bandMember1Album= beatles[key3][0]
+
+let bandStartDate= beatles[key2].formed
+let bandEndDate= beatles[key2].disbanded
+
+// Access band member 2 (aka Paul) info: need name, birth year, year started and ended band career
+let bandMember2= beatles[key1][1]
+let bandMember2Name= bandMember2.name
+let bandMember2Birth= bandMember2.birth
+let bandMember2Album= beatles[key3][1]
+
+// Access band member 3 (aka John) info: need name, birth year, year started and ended band career
+let bandMember3= beatles[key1][2]
+let bandMember3Name= bandMember3.name
+let bandMember3Birth= bandMember3.birth
+let bandMember3Album= beatles[key3][2]
+
+// Access band member 2 (aka Paul) info: need name, birth year, year started and ended band career
+let bandMember4= beatles[key1][3]
+let bandMember4Name= bandMember4.name
+let bandMember4Birth= bandMember4.birth
+let bandMember4Album= beatles[key3][3]
+
+//Output for each bandmember
+console.log(`${bandMember1Name} was in the Beatles from ${bandStartDate} to ${bandEndDate}. He was born in ${bandMember1Birth}. He contributed heavily to the ${bandMember1Album} Album.`)
+
+console.log(`${bandMember2Name} was in the Beatles from ${bandStartDate} to ${bandEndDate}. He was born in ${bandMember2Birth}. He contributed heavily to the ${bandMember2Album} Album.`)
+
+console.log(`${bandMember3Name} was in the Beatles from ${bandStartDate} to ${bandEndDate}. He was born in ${bandMember3Birth}. He contributed heavily to the ${bandMember3Album} Album.`)
+
+console.log(`${bandMember4Name} was in the Beatles from ${bandStartDate} to ${bandEndDate}. He was born in ${bandMember4Birth}. He contributed heavily to the ${bandMember4Album} Album.`)
